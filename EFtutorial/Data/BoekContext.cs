c91204da5dec37c5a84921cdd35f67d32c6b0f44
tvdewiel <tvdewiel@hotmail.com>
2023-10-17 13:52:48 +0200

    Update BoekContext.cs

--- EFtutorial/Data/BoekContext.cs
+++ EFtutorial/Data/BoekContext.cs
@@ -8,12 +8,17 @@ namespace EFtutorial.Data
 {
     public class BoekContext : DbContext
     {
+       //public static readonly ILoggerFactory factory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
         public DbSet<Boek> Boeken { get; set; }
         public DbSet<Auteur> Auteurs { get; set; }
         public DbSet<Uitgeverij> Uitgeverijen { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=lenovo-pc\sqlexpress;Initial Catalog=boekDB;Integrated Security=True");
+           // optionsBuilder.UseLoggerFactory(factory)  //tie-up DbContext with LoggerFactory object
+           //.EnableSensitiveDataLogging()
+           optionsBuilder.UseSqlServer(@"Data Source=NB21-6CDPYD3\SQLEXPRESS;Initial Catalog=EFtutorial;Integrated Security=True;TrustServerCertificate=True")
+                .LogTo(Console.WriteLine);
         }
     }
 }
